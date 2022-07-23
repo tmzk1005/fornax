6006d6e6655ff293bbc41257dfb05d2ac5b21f3b
zoukang <zoukang89105@163.com>
2022-07-23 13:33:47 +0800

    优化gateway模块ExceptionHandleHttpApiFilter

--- gateway/src/main/java/zk/fornax/gateway/filter/ExceptionHandleHttpApiFilter.java
+++ gateway/src/main/java/zk/fornax/gateway/filter/ExceptionHandleHttpApiFilter.java
@@ -1,6 +1,7 @@
 package zk.fornax.gateway.filter;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
+import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Mono;
 
 import zk.fornax.gateway.exception.ResponseStatusException;
@@ -9,19 +10,23 @@ import zk.fornax.http.core.exchange.WebExchange;
 import zk.fornax.http.core.filter.HttpApiFilter;
 import zk.fornax.http.core.filter.HttpFilterChain;
 
+@Log4j2
 public class ExceptionHandleHttpApiFilter implements HttpApiFilter {
 
     @Override
     public Mono<Void> filter(WebExchange webExchange, HttpFilterChain chain) {
         return chain.filter(webExchange).onErrorResume(throwable -> {
+            log.error("An exception did not handled by filters before ExceptionHandleHttpApiFilter.", throwable);
+            HttpResponseStatus httpResponseStatus;
+            String message;
             if (throwable instanceof ResponseStatusException responseStatusException) {
-                return ResponseHelper.sendJson(
-                    webExchange.getResponse(),
-                    responseStatusException.getHttpResponseStatus(),
-                    responseStatusException.getMessage()
-                );
+                httpResponseStatus = responseStatusException.getHttpResponseStatus();
+                message = responseStatusException.getMessage();
+            } else {
+                httpResponseStatus = HttpResponseStatus.INTERNAL_SERVER_ERROR;
+                message = httpResponseStatus.reasonPhrase();
             }
-            return ResponseHelper.sendJson(webExchange.getResponse(), HttpResponseStatus.INTERNAL_SERVER_ERROR);
+            return ResponseHelper.sendJson(webExchange.getResponse(), httpResponseStatus, message);
         });
     }
 
